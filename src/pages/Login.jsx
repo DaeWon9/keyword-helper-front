a29c79fc9858ea166f8fd403656bd451264490b7
000wan <87213416+000wan@users.noreply.github.com>
2023-02-17 22:45:26 +0900

    Create keyword box component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import KeywordBox from "../components/KeywordBox";
 
 const Login = () => {
-  return <>login</>;
+  return (
+    <div>
+      <p>Hello</p>
+      {Array.apply(null, Array(15)).map((e, id) => (
+        <KeywordBox colorID={id} text={`${id}st Keyword`} />
+      ))}
+    </div>
+  );
 };
 
 export default Login;
