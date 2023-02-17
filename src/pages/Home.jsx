c5032bbb2b14c9457465455aeba51283dbf0f5ec
DaeWon9 <qkreodnjs97@naver.com>
2023-02-17 21:06:40 +0900

    Add Home style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import "./Home.css"
 
 const Home = () =>{
     return(
-        <>
-            home
-        </>
+        <div className="home">
+            <div className="home-left">
+
+            </div>
+            <div className="home-right">
+                <div className="home-header">
+                    
+                </div>
+                <div className="home-body">
+                    <div className="home-body-chat">
+                        <div className="home-body-chat-content">
+
+                        </div>
+                        <div className="home-body-chat-send">
+
+                        </div>
+                    </div>
+                    <div className="home-body-menu">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
