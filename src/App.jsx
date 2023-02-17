c387eeb86dae0e6e96635098b9a5141282211a6d
DaeWon9 <qkreodnjs97@naver.com>
2023-02-17 21:38:31 +0900

    Add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <Login />} />
-          <Route path="/chat" element={ <Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
